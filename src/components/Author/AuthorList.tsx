1aa752215f6bce271c3b68a87d17db3ef4c90e7a
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-05-20 03:49:32 +0530

    Author UX, feather icons added

--- src/components/Author/AuthorList.tsx
+++ src/components/Author/AuthorList.tsx
@@ -5,7 +5,7 @@ import {Authors} from "../../types/LibraryTypes";
 
 const AuthorList : React.FC = () => {
     const authorsList: Authors [] = [{name:'Author 1'},{name:'Author 2'},{name:'Author 3'}];
-    // const authors: Authors [] = [];
+    // const authorsList: Authors [] = [];
     const [authors,setAuthor] = useState(authorsList);
     const renderAuthorList = () =>{
         if(authors.length===0){
@@ -18,15 +18,12 @@ const AuthorList : React.FC = () => {
 
     return(
         <Row>
-
             <Col>
                 {authors.length ===0 && <label className='empty-list mb-2'>No authors listed here</label>}
+                <ul className='author-ul ml-0 pl-0'>
+                    {renderAuthorList()}
+                </ul>
             </Col>
-
-            <ul className='author-ul'>
-               {renderAuthorList()}
-            </ul>
-
         </Row>
     );
 }
