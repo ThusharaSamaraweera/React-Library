3a4a443a4c1f15d13889716cd9353c1ab04025ca
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-07-14 17:07:52 +0530

    completed menu-nav

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Col, Image, Row} from "react-bootstrap";
 import WelcomeImage from '../assets/images/ezgif.com-gif-maker.webp';
+import MenuNav from "./MenuNav";
 
 const Welcome: React.FC = () => {
   return (
     <Row className='welcome-section'>
-      <Col xs={12} className='text-center py-2'>
-        <h1>My Library</h1>
+      <Col xs={12} className='py-2'>
+        <Row>
+          <Col xs={5} className='title'>
+            <h1>My Library</h1>
+          </Col>
+          <Col xs={7} className='menu'>
+            <MenuNav/>
+          </Col>
+        </Row>
       </Col>
       <Col xs={12} className='welcome-image px-0'>
         <Image src={WelcomeImage} alt='library photo'/>
